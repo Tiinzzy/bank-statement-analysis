f34afde4ba163454f10ebade3a076a50495e5434
Tina <tinzzy.cracked@yahoo.com>
2022-12-12 11:29:17 -0500

    can successfully display grid from CSV data in public folder

--- front-end/src/components/ExpenceGrid.js
+++ front-end/src/components/ExpenceGrid.js
@@ -1,22 +1,38 @@
 import React from "react";
 
+import { DataGrid } from '@mui/x-data-grid';
+
 import { shared } from './shared';
+import { getDataFromPublic, getColumns } from "./functions";
 
 class ExpenceGrid extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            columns: [],
+            rows: []
 
         };
-
         this.callExpenceGrid = this.callExpenceGrid.bind(this);
         shared.callExpenceGrid = this.callExpenceGrid;
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let data = await getDataFromPublic();
+        let columns = getColumns(data[0]);
+     
+        let id = 0;
+        for (let r in data) {
+            data[r].id = id;
+            id += 1;
+        }
+        
+        this.setState({ columns: columns, rows: data });
+
+        shared.callBody({ data })
     }
 
-    callExpenceGrid(message) {        
+    callExpenceGrid(message) {
         console.log(message);
         if (message.action === 'update-data') {
             console.log('now i have to update the whole grid data');
@@ -25,8 +41,18 @@ class ExpenceGrid extends React.Component {
         }
     }
 
+
     render() {
-        return <div style={{ padding: 5, width: 200, height: 200, background: '#eaeaea', height: 500 }}>This is the GRID</div>;
+        return (
+            <div>
+                <DataGrid
+                    style={{ height: 700, width: '100%' }}
+                    hideFooterPagination={true}
+                    hideFooter={true}
+                    rows={this.state.rows}
+                    columns={this.state.columns} />
+            </div>
+        );
     }
 }
 
