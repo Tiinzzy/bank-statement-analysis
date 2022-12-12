c73f62c66c9931956413b59b4f51da5765d671c9
Tina <tinzzy.cracked@yahoo.com>
2022-12-12 09:58:19 -0500

    API to read cvs file from public and save in data.json

---
+++ back-end/data-services.js
@@ -0,0 +1,13 @@
+const JSONdb = require('simple-json-db');
+const db = new JSONdb('./data.json');
+
+exports.saveCsvFile = (req) => {
+    let data = req.query.data;
+    if (typeof data === "object") {
+        db.JSON(data);
+        db.sync();
+        return { success: true }
+    } else {
+        return { success: false }
+    }
+}
--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,13 @@
-// server/index.js
-
 const express = require("express");
+const dataServices = require("data-services.js");
 
 const PORT = process.env.PORT || 8888;
 
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  app.post("/save-csv-file", (req, res) => {
+    console.log('Saving CSV File');
+    res.json(dataServices.saveCsvFile(req));
+  })
 });
