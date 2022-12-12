2942bd27c13a765cb2afac986423caac3b43296e
Tina <tinzzy.cracked@yahoo.com>
2022-12-12 09:17:43 -0500

    Created components with constants

---
+++ front-end/src/components/constants.js
@@ -0,0 +1,15 @@
+export const constants = {
+    help: [
+        "First go to Upload File at the top of the page",
+        "Select a file to uploead using the button",
+        "The file must be CSV to be read",
+        "When your file is uploaded, you will see a message that the file is compatibale or not",
+        "If a previous file exist, your new upload will override that and you old data will be lost",
+        "If youre satisfied with your file click on save button",
+        "After saving the file new data will be available and you can see them in the grid",
+        "Then you have the option to set categories for each row",
+        "After setting categories you can see the option of filters and charts to see your spending habits"
+    ],
+    daysOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    categories: ['All', 'Medicine', 'None', 'Commute', 'Entertainment', 'Groceries', 'Houseware', 'Clothing', 'Utilities', 'Misc', 'Dining', 'Online Shopping'].sort()
+}
