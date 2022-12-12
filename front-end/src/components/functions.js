56fbf9f1d71d65778fd10a4e93a695d2d77918d5
Tina <tinzzy.cracked@yahoo.com>
2022-12-12 11:27:18 -0500

    function to get columns

--- front-end/src/components/functions.js
+++ front-end/src/components/functions.js
@@ -14,3 +14,27 @@ export async function getDataFromPublic() {
         }
     });
 }
+
+export function getColumns(row) {
+    var defaultColumns = [];
+
+    for (var c in row) {
+        let col = { field: c, headerName: c, width: (c === 'DESC' ? 600 : 200) };
+        if (col.field === 'AMOUNT') {
+            col.type = 'number';
+            col.align = 'right';
+        }
+        defaultColumns.push(col);
+    }
+
+    let columns = [];
+    defaultColumns.forEach(c => {
+        if (c.field === 'id') {
+            columns.unshift(c);
+        } else {
+            columns.push(c);
+        }
+    });
+
+    return columns;
+}
