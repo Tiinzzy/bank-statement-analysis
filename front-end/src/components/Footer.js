a24b53f30c837935095b3228be418d7b94cbd2dd
Tina <tinzzy.cracked@yahoo.com>
2022-12-13 11:54:50 -0500

    fixed some styling

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -12,7 +12,8 @@ class Footer extends React.Component {
 
     render() {
         return (
-            <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', textAlign: 'center', fontSize: '10px' }}>
+            <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', textAlign: 'center', fontSize: '10px', borderTop: 'solid 1px black',
+            paddingTop: 6, margin: '10px 300px 10px 300px' }}>
          Designed by Tina Vatanabadi, Copyright 2022.
             </Box>
         );
