169eec39deda1c813aa76efd3a76ffac149c25ab
Tina <tinzzy.cracked@yahoo.com>
2022-12-14 16:28:54 -0500

    snackbar to inform new file have been uploaded successfully

--- front-end/src/components/Body.js
+++ front-end/src/components/Body.js
@@ -5,6 +5,8 @@ import Dialog from "@mui/material/Dialog";
 
 import ExpenceGrid from "./ExpenceGrid";
 import UploadFileDialog from "./UploadFileDialog";
+import Snackbar from '@mui/material/Snackbar';
+import SnackbarContent from '@mui/material/SnackbarContent'
 
 import { shared } from './shared';
 import { constants } from './constants';
@@ -16,10 +18,12 @@ class Body extends React.Component {
         super(props);
         this.state = {
             data: [],
-            openDialog: false
+            openDialog: false,
+            openSnack: false
         };
         this.callBody = this.callBody.bind(this);
         this.handleCloseDialog = this.handleCloseDialog.bind(this);
+        this.handleCloseSnack = this.handleCloseSnack.bind(this);
         shared.callBody = this.callBody;
     }
 
@@ -30,15 +34,19 @@ class Body extends React.Component {
 
     callBody(message) {
         if (message.action === 'new-file-uploaded-successfully') {
-            this.setState({ openDialog: true })
+            this.setState({ openDialog: true, newData: message.data })
         }
-        else {
-            this.setState({ data: message.data })
+        else if (message.action === 'new-uploaded-file-saved') {
+            this.setState({ openSnack: true })
         }
     }
 
     handleCloseDialog() {
         this.setState({ openDialog: false })
+    }
+
+    handleCloseSnack() {
+        this.setState({ openSnack: false, message: null });
     }
 
     render() {
@@ -58,10 +66,19 @@ class Body extends React.Component {
                     onClose={() => this.handleCloseDialog()}
                     open={this.state.openDialog}
                     maxWidth='xl' fullWidth={true}>
-                    <UploadFileDialog close={this.handleCloseDialog} data={this.state.data}/>
+                    <UploadFileDialog close={this.handleCloseDialog} data={this.state.newData} />
                 </Dialog>}
 
-            </Box>);
+                <Snackbar
+                    anchorOrigin={{ vertical: 'top', horizontal: 'center', }}
+                    open={this.state.openSnack}
+                    autoHideDuration={2000}
+                    onClose={this.handleCloseSnack}>
+                    <SnackbarContent style={{ backgroundColor: '#63A355', color: 'white', fontWeight: 'bold' }}
+                        message={<div style={{ textAlign: 'center', width: 400 }}>New File Successfully Uploaded</div>} />
+                </Snackbar>
+            </Box>
+        );
     }
 }
 
