29b764c5996655ccf0d29ea74b6824175b79d6a8
Tina <tinzzy.cracked@yahoo.com>
2022-12-14 09:15:48 -0500

    dialog component for new uploaded file to display to user

---
+++ front-end/src/components/UploadFileDialog.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+
+import { shared } from './shared';
+
+class UploadFileDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+
+    render() {
+        return (
+            <Box>
+                
+            </Box>);
+    }
+}
+
+export default UploadFileDialog;
