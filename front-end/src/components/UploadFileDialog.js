5a5911d5c06006f025f0e16a21a4803471bf206d
Tina <tinzzy.cracked@yahoo.com>
2022-12-14 16:30:57 -0500

    if file is not compatibale will inform the user, if compatible will allow user to save

--- front-end/src/components/UploadFileDialog.js
+++ front-end/src/components/UploadFileDialog.js
@@ -6,8 +6,10 @@ import Button from '@mui/material/Button';
 import DialogTitle from "@mui/material/DialogTitle";
 import Divider from "@mui/material/Divider";
 import DialogActions from "@mui/material/DialogActions";
+import Typography from "@mui/material/Typography";
 
-import { getColumns } from "./functions";
+import { getColumns, saveCsvFile } from "./functions";
+import { shared } from "./shared";
 
 class UploadFileDialog extends React.Component {
     constructor(props) {
@@ -21,9 +23,10 @@ class UploadFileDialog extends React.Component {
     }
 
     componentDidMount() {
-        let data = this.refreshData(this.state.data);
+        let data = this.refreshData(Object.values(this.state.data));
         let columns = getColumns(data[0]);
         this.setState({ columns });
+        console.log(this.state.data)
     }
 
     refreshData(data) {
@@ -38,8 +41,10 @@ class UploadFileDialog extends React.Component {
         return data;
     }
 
-    save(e) {
+    async save() {
+        let storedSuccessfully = await saveCsvFile(this.state.data);
         this.state.close();
+        shared.callBody({ action: storedSuccessfully ? 'new-uploaded-file-saved' : 'problem-uploading-data' })
     }
 
     cancelAndClose(e) {
@@ -49,16 +54,33 @@ class UploadFileDialog extends React.Component {
     render() {
         return (
             <Box>
-                <DialogTitle>Would You Like to Save the Following Data?</DialogTitle>
+                {this.state.rows.length > 100 ? <DialogTitle>Can't Save the selected CSV file!</DialogTitle>
+                    : <DialogTitle>Would You Like to Save the Following Data?</DialogTitle>}
                 <Divider />
-                <Box style={{ paddingTop: 10, paddingBottom: 10, paddingLeft: 10, paddingRight: 10 }}>
-                    <DataGrid
-                        style={{ height: 400, width: 1335 }}
-                        hideFooterPagination={true}
-                        hideFooter={true}
-                        rows={this.state.rows}
-                        columns={this.state.columns} />
+                <Box style={{ paddingTop: 10, paddingBottom: 10, paddingLeft: 25, paddingRight: 10 }}>
+                    {this.state.rows.length > 100 ?
+                        <Box p={2}>
+                            <Typography variant="body1" component="div" style={{ lineHeight: '160%' }}>
+                                Number of rows of the file  you have uploaded is <span style={{ color: "red" }}> {this.state.rows.length}</span>.
+                                <br />
+                                PLease upload a file with less than 100 rows to be able to save it.
+                            </Typography>
+                        </Box> :
+                        <DataGrid
+                            style={{ height: 400, width: 1320 }}
+                            hideFooterPagination={true}
+                            hideFooter={true}
+                            rows={this.state.rows}
+                            columns={this.state.columns} />}
                 </Box>
+
+                {this.state.rows.length > 100 ? null :
+                    <Box variant="body1" style={{ marginLeft: 25, marginTop: 10, marginBottom: 10 }}>
+                        <Box> * Number of Rows in Uploaded Data: <span style={{ fontWeight: 'bold' }}>{this.state.rows.length}</span></Box>
+                        <br />
+                        <Box> * Saving the New File Will Remove any Pre-existing Data.</Box>
+                    </Box>}
+
                 <Divider />
                 <DialogActions>
                     <Button onClick={(e) => this.cancelAndClose(e)} variant="outlined" color="error"> Cancel </Button>
