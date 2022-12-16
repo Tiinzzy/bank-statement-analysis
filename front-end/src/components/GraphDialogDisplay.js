f9b8cb5e08661191e789fb5fef67ce37edabc2d0
Tina <tinzzy.cracked@yahoo.com>
2022-12-16 11:22:55 -0500

    component for displaying graphs

---
+++ front-end/src/components/GraphDialogDisplay.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Chart } from "react-google-charts";
+import Box from '@mui/material/Box';
+
+export default function GraphDialogDisplay(props) {
+    var options = {
+        isStacked: true,
+        bars: "horizontal",
+        bar: { groupWidth: '75%' },
+        legend: { position: 'top', maxLines: 3 },
+        chartArea: {'width': '85%', 'height': '75%'}
+    };
+
+    return (
+        <Box style={{ margin: 20 }}>
+            {props.graphIndex === 1 && <Chart
+                chartType="Bar"
+                height={window.innerHeight / 2}
+            />}
+
+            {props.graphIndex === 2 && <Chart
+                chartType="Bar"
+                height={window.innerHeight / 2}
+            />}
+
+            {props.graphIndex === 3 && <Chart
+                chartType="ColumnChart"
+                height={window.innerHeight / 2}
+                options={options}
+            />}
+        </Box>
+    );
+};
