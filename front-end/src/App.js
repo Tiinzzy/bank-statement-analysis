93d84a57328a329cd91a2db249ae57e668d839da
Tina <tinzzy.cracked@yahoo.com>
2023-08-20 10:01:17 -0400

    fixed and created build to work with

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter } from "react-router-dom";
 import { createBrowserHistory } from 'history';
-
-import axios from 'axios';
 
 import Header from './components/Header';
 import ChartsAndFilters from './components/ChartsAndFilters';
 import Body from './components/Body';
 import Footer from './components/Footer';
-
-const customHistory = createBrowserHistory({ basename: '/bank' });
 
 
 const theme = createTheme({
@@ -24,7 +19,7 @@ const theme = createTheme({
   },
 });
 
-function RealApp() {
+export default function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
@@ -36,16 +31,3 @@ function RealApp() {
     </div>
   );
 };
-
-
-export default function App() {
-  axios.get('/bank/bank-statement/test').then(resp => {
-    console.log(resp.data);
-  });
-  return (
-    // <BrowserRouter basename="/db">
-    <RealApp />
-    // </BrowserRouter>
-    // <h1>BANK</h1>
-  )
-}
